06a795662f7170a5aa2bf24523e692ad85807bd0
Hermit-killer <793448166@qq.com>
2024-07-12 11:13:53 +0800

    完成了以太网层的一个处理。

--- tnet_core/tnet_core.c
+++ tnet_core/tnet_core.c
@@ -1,5 +1,6 @@
 #include "tnet_core.h"
 #define min(a,b) ((a)<(b)? (a) : (b))
+static uint8_t netif_mac[TNET_MAC_ADDR_SIZE];
 
 static tnet_packet_t tx_packet,rx_packet;
 
@@ -28,4 +29,50 @@ static void remove_header(tnet_packet_t * packet, uint16_t header_size){
 static void truncate_header(tnet_packet_t * packet, uint16_t size){
     packet->size = min(packet->size,size);    
 
+}
+
+static tnet_err_t ethernet_init(void){
+    tnet_err_t err = tnet_driver_open(netif_mac);
+    if(err<0) return err;
+    return TNET_ERR_OK;
+}
+
+static tnet_err_t ethernet_out_to(tnet_protocal_t protocal, 
+                                const uint8_t* mac_addr,
+                                tnet_packet_t* packet){
+    tether_hdr_t * ether_hdr;
+    add_header(packet,sizeof(tether_hdr_t));
+    ether_hdr = (tether_hdr_t *) packet->data;
+    memcpy(ether_hdr->dest,mac_addr,TNET_MAC_ADDR_SIZE);
+    memcpy(ether_hdr->src,netif_mac,TNET_MAC_ADDR_SIZE);
+    ether_hdr->protocal = protocal;
+    return tnet_driver_xfer(packet);
+}
+
+static void ethernet_in(tnet_packet_t * packet){
+    tether_hdr_t * ether_hdr; 
+    if(packet->size<=sizeof(tether_hdr_t)){
+        return;
+    }
+    switch(ether_hdr->protocal){
+        case TNET_PROTOCAL_ARP:
+            break;
+        case TNET_PROTOCAL_IP:
+            break;
+    }
+}
+
+static void ethernet_poll(void){
+    tnet_packet_t * packet;
+    if(tnet_driver_read(&packet)==TNET_ERR_OK){
+        ethernet_in(packet);
+    }
+}
+
+void tnet_init(void){
+    ethernet_init();
+}
+
+void tnet_poll(void){
+    ethernet_poll();
 }
--- tnet_core/tnet_core.h
+++ tnet_core/tnet_core.h
@@ -2,11 +2,25 @@
 #include <stdint.h>
 #define TNET_CORE_H
 #define TNET_CFG_PACKET_MAX_SIZE    1516
+#define TNET_MAC_ADDR_SIZE  8
+
+#pragma pack(1)
+typedef struct _tether_hdr_t{
+    uint8_t dest[TNET_MAC_ADDR_SIZE];
+    uint8_t src[TNET_MAC_ADDR_SIZE];
+    uint16_t protocal;
+}tether_hdr_t;
+#pragma pack(0)
 
 typedef enum _tnet_err_t{
     TNET_ERR_OK = 0,
     TNET_ERR_IO = -1,
 }tnet_err_t;
+
+typedef enum _tnet_protocal_t{
+    TNET_PROTOCAL_ARP = 0x0806,
+    TNET_PROTOCAL_IP = 0x0800,
+}tnet_protocal_t;
 
 typedef struct _tnet_packet_t{
     uint16_t size;
